17381f7f1bc639ec758a92acd9890e7e17dbb8c8
M.RAY <a.mray47@yahoo.fr>
2021-10-28 15:39:09 +0800

    Add FindOne function

--- myApp.js
+++ myApp.js
@@ -41,9 +41,12 @@ const findPeopleByName = (personName, done) => {
     done(null, people);
   });
 };
-
+/**FindOne function */
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, (err, data)=> {
+    if(err) return console.error(err);
+    done(null, data);
+  })
 };
 
 const findPersonById = (personId, done) => {
